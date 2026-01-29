162106295c5fc013ad0809904b8c8b8463a18cc3
askal-thapa <askalthapa91@gmail.com>
2026-01-29 20:46:55 +0545

    Remove env files and add .gitignore

--- backend/app/models/state.py
+++
@@ -1,8 +0,0 @@
-from typing import TypedDict, Annotated, List, Union
-from langchain_core.messages import BaseMessage
-import operator
-
-class BotState(TypedDict):
-    messages: Annotated[List[BaseMessage], operator.add]
-    mood: str
-    intent: str
